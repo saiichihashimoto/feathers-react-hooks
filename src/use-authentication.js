cbd03ac57a4dc70db2dec382cedcc60a6f2cff12
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-08-14 22:55:07 -0700

    refactor(hooks): use-event-emitter

--- src/use-authentication.js
+++ src/use-authentication.js
@@ -1,8 +1,8 @@
 import { useCallback, useDebugValue, useEffect } from 'react';
+import useEventEmitter from 'use-event-emitter';
 
 import useSetting from './use-setting';
 import { HAS_ATTEMPTED_AUTHENTICATION, IS_AUTHENTICATED } from './constants';
-import { useEventEmitter } from './hooks';
 
 function useAuthentication(app) {
 	const [isAuthenticated, setIsAuthenticated] = useSetting(app, IS_AUTHENTICATED, null);
